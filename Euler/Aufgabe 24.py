159425b23f7598b68246c46c27244673b0f5db4b
Max <mrxam@gmx.de>
2022-11-14 15:31:24 +0100

    Files

---
+++ Euler/Aufgabe 24.py
@@ -0,0 +1,15 @@
+import eulerLib as eL
+c = 0
+numbers = [False] * 10
+
+
+
+def it(x, y):
+    for n in range(0, 10):
+        if not numbers[n]:
+            numbers[n]==True
+            y += 1
+            return (F'{it(x,y)}')
+
+
+
