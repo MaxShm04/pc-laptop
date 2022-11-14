13ee7ef843542d10ccb669cbeb22f754f7bb80e7
Max <mrxam@gmx.de>
2022-11-14 14:37:48 +0100

    Files

---
+++ Euler/Aufgabe 1.py
@@ -0,0 +1,19 @@
+msg = "Max"
+print(msg)
+msg = "Schmitt"
+print(msg)
+
+
+#print length
+char_cnt = len("Hello world")
+print(char_cnt)
+
+print("first\tline\nsecond\tline")
+
+
+
+print('Prille: "Es lebt Jim, aber nicht so wie wir es kennen"')
+print("it´s time  to pratice")
+print("Mir \
+      geht \
+      es gut")
--- Euler/main.py
+++ Euler/main.py
@@ -1,25 +1,6 @@
 import eulerLib as eL
 
 """
-msg = "Max"
-print(msg)
-msg = "Schmitt"
-print(msg)
-
-
-#print length
-char_cnt = len("Hello world")
-print(char_cnt)
-
-print("first\tline\nsecond\tline")
-
-
-
-print('Prille: "Es lebt Jim, aber nicht so wie wir es kennen"')
-print("it´s time  to pratice")
-print("Mir \
-      geht \
-      es gut")
 
 Aufgabe 2
 
