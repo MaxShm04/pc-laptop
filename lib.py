15a83ad81fbacb5c2e4c3420c573ef85d3cc1147
Max <mrxam@gmx.de>
2022-12-05 11:21:43 +0100

    Files

--- lib.py
+++ lib.py
@@ -1,17 +1,19 @@
 import os
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-mainRoot = ROOT_DIR[:int(ROOT_DIR.find("Python_Hausaufgaben"))]
-#print("ROOTDIR " + ROOT_DIR)
-mainRoot += "duzzelLibrary"
 import sys
-#print(mainRoot+"duzzelLibrary")
-sys.path.append(os.path.abspath(mainRoot+"duzzelLibrary"))
-SYS_ROOT_DIR = os.path.dirname(os.path.abspath(sys.executable))
-SYS_ROOT_DIR += "\lib"
 import shutil
-shutil.copy(mainRoot+"\eulerLib.py", SYS_ROOT_DIR)
-print("Sync successful")
+def sync(file):
+    '''
+    write __file__ as argument
 
-
+    :param file: __file__
+    :return: nothing
+    '''
+    ROOT_DIR = os.path.dirname(os.path.abspath(file))
+    mainRoot = ROOT_DIR[:int(ROOT_DIR.find("Python_Hausaufgaben"))]
+    mainRoot += "duzzelLibrary"
+    sys.path.append(os.path.abspath(mainRoot + "duzzelLibrary"))
+    SYS_ROOT_DIR = os.path.dirname(os.path.abspath(sys.executable))
+    SYS_ROOT_DIR += "\lib"
+    shutil.copy(mainRoot + "\eulerLib.py", SYS_ROOT_DIR)
+    #print("Sync successful")
 
