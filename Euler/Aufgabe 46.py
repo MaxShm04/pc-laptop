45d2850a490265ae9c479e052887481c23d6593f
Max <mrxam@gmx.de>
2022-12-08 11:42:00 +0100

    Files

--- Euler/Aufgabe 46.py
+++ Euler/Aufgabe 46.py
@@ -6,27 +6,33 @@ import math
 
 prims = [3, 7, 9, 11]
 ps, pe = 10, 10
+
+
 def main(s, e):
     global ps
     global pe
     for n in range(s, e):
-        #print(n)
+        # print(n)
         if n % 2 == 1 and not eL.isPrim(n):
             state = False
             out = []
             for l in eL.givePrimes(n, start=ps):
                 prims.append(l)
             ps = n
-            #print(prims)
+            # print(prims)
             for p in prims:
                 r = n - p
-                print(F"{r} = {n} - {p}")
-                if r != 0 and r == (2 * (math.pow(a, 2) for a in range(1, int(r)))):
-                    print(F"state True")
-                    state = True
+                # print(F"{r} = {n} - {p}")
+                for a in range(1, int(r)):
+                    if r == (2 * math.pow(a, 2)):
+                        if n > 5000:
+                            print(F"{n} state True")
+                        state = True
+                        break
+                    else:
+                        out = [n, r, p]
+                if state:
                     break
-                else:
-                    out = [n, r, p]
             if not state:
                 return out
     return False
@@ -37,7 +43,7 @@ e = 1000
 while True:
     x = main(st, e)
     if not x:
-        st, e = e, e*10
+        st, e = e, e + 100
         print(F"Start {st}, end {e}")
     else:
         print(x)
