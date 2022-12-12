1268a4dfd767c5a57d08181bdb892c81a8be55b3
Max <mrxam@gmx.de>
2022-12-12 13:07:09 +0100

    Files

---
+++ Euler/Aufgabe 76.py
@@ -0,0 +1,16 @@
+import lib
+import math
+from datetime import datetime
+
+lib.sync(r'C:\Users\MrXam\PycharmProjects\duzzelLibrary\eulerLib.py')
+import eulerLib as eL
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    time = datetime.now()
+    main()
+    print(datetime.now() - time)
---
+++ Euler/Aufgabe 81.py
@@ -0,0 +1,30 @@
+import lib
+import math
+from datetime import datetime
+
+lib.sync(r'C:\Users\MrXam\PycharmProjects\duzzelLibrary\eulerLib.py')
+import eulerLib as eL
+
+
+def main():
+    text = []
+    sum = 0
+    x = 0
+    y = 0
+    with open('p081_matrix.txt', 'r') as file:
+        str = file.read().rstrip()
+        text = str.split("\n")
+    for i, strin in enumerate(text):
+        text[i] = strin.split(",")
+    check = [[-1 for n in range(0, 80)]for n in range(0, 80)]
+    while True:
+        if check[y][x+1] == -1
+    print(text)
+    print(check)
+    return
+
+
+if __name__ == '__main__':
+    time = datetime.now()
+    main()
+    print(datetime.now() - time)
