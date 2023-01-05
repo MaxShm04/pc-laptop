eb7369d44c8f90ac25f365bd1dbc6481d6bd6e1f
Max <mrxam@gmx.de>
2023-01-05 01:47:33 +0100

    Files

---
+++ recBot.py
@@ -0,0 +1,23 @@
+import lib
+import math
+import time
+from datetime import datetime
+
+lib.sync(r'C:\Users\MrXam\PycharmProjects\duzzelLibrary\eulerLib.py')
+import eulerLib as eL
+
+
+import pyautogui
+import keyboard
+while True:
+    res = pyautogui.locateOnScreen("sc1.png")
+    if res is not None:
+        for n in range(1, 5):
+            time.sleep(n)
+        print(res)
+        x, y = pyautogui.center(res)
+        x1, y1 = pyautogui.position()
+        pyautogui.leftClick(x,y)
+        pyautogui.moveTo(x1, y1)
+    if keyboard.is_pressed("n"):
+        break
