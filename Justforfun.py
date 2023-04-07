472dc831707da629f4d4dc40dec4114048bb4fba
Max <mrxam@gmx.de>
2023-04-07 17:12:25 +0200

    Files

--- Justforfun.py
+++ Justforfun.py
@@ -7,10 +7,25 @@ import eulerLib as eL
 
 
 def main():
-        x = [0, 1, 0]
-
-    return
-
+    ter = 1.45
+    ausgabe = 3.75
+    rate = 40
+    dauer = 7
+    normal = 0
+    mitkosten = 0
+    pa = 8
+    print(f"gespart: {12*rate*dauer}")
+    for i in range(dauer):
+        normal += 12*rate
+        normal *= 1+pa/100
+        normal -= normal*ter/100
+    print(f"normal: {normal:.2f}")
+    for i in range(dauer):
+        mitkosten += (12 * (rate-rate*ausgabe/100))
+        mitkosten *= 1 + pa / 100
+        mitkosten -= (normal * (ter / 100))
+    print(f"mitkosten: {mitkosten:.2f}")
+    print(F"{normal-mitkosten:.2f}")
 
 if __name__ == '__main__':
     time = datetime.now()
