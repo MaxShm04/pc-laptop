af7f3d16e03d524f1627bf844c2f3b88be1ef926
Max <mrxam@gmx.de>
2025-02-05 17:31:57 -0800

    Add lab implementation and project configuration updates
    Introduced multiple Python scripts (`lab00.py`, `lab01.py`, `class.py`) implementing basic functionalities for CS 9 labs, including item management and string manipulations. Additionally, updated project configurations to include Python 3.11 SDK and module settings, enabling seamless development.

--- ucsb/sc.py
+++ ucsb/sc.py
@@ -25,28 +25,107 @@ def recursiveBinarySearch(intList, target):
                 return recursiveBinarySearch(intList[:mid], target)
             else:
                 return recursiveBinarySearch(intList[mid+1:], target)
-"""
 
 class A(Exception):
-	pass
+    pass
+class B(Exception):
+    pass
+class C(B):
+    pass
+class D(C):
+    pass
+def f(n):
+    try:
+        if n < 0:
+            raise A()
+        if n >= 0 and n < 5:
+            raise B()x
+        if n >= 5 and n < 10:
+            raise C()
+        if n >= 10 and n < 15:
+            raise D()
+        raise Exception()
+    except A:
+        print("A")
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+    except Exception:
+        print("Exception")
 
-class B(A): # B inherits from A (B IS-A A type)
-	pass
+f(2)
 
-class C(Exception):
-	pass
+class Vehicle:
+    def __init__(self, name, topSpeed):
+        self.name = name
+        self.topSpeed = topSpeed
 
-try:
-	x = int(input("Enter a positive number: "))
-	if x < 0:
-		raise C() # Change this to A() and C() and observe...
-except C:
-	print("Exception of type C caught")
-except A:
-	print("Exception of type A caught")
-except B:
-	print("Exception of type B caught") # Will never get called
-except Exception:
-	print("Exception of type Exception caught")
+    def setName(self, name):
+        self.name = name
 
-print("Resuming execution")
+    def setTopSpeed(self, topSpeed):
+        self.topSpeed = topSpeed
+
+    def getName(self):
+        return self.name
+    def getTopSpeed(self):
+        return self.topSpeed
+    def getInfo(self):
+        return "Name: {}, Top Speed: {} mph".format(self.name, self.topSpeed)
+
+
+class Plane(Vehicle):
+    def __init__(self, name, topSpeed, altitude):
+        super().__init__(name, topSpeed)
+        self.altitude = a   ltitude
+
+    def getInfo(self):
+        return super().getInfo() + ", Altitude: {} ft".format(self.altitude)
+
+    def __lt__(self, rhs):
+        return self.altitude < rhs.altitude
+
+
+p1 = Plane("Dreamliner", 594, 33000)
+p2 = Plane("900LX", 667, 38000)
+assert p1.getInfo() == "Name: Dreamliner, Top Speed: 594 mph, Altitude: 33000 ft"
+assert p2.getInfo() == "Name: 900LX, Top Speed: 667 mph, Altitude: 38000 ft"
+assert (p1 < p2) == True
+assert (p2 < p1) == False
+
+"""
+def findNum(num, intList):
+    if len(intList)== 0:
+        return False
+    else:
+        mid = len(intList) // 2
+        if intList[mid] == num:
+            return True
+        else:
+            if num < mid:
+                return findNum(num, intList[:mid])
+            else:
+                return findNum(num, intList[mid+1:])
+
+
+assert findNum(1, []) == False
+assert findNum(3, [1,2,3]) == True
+assert findNum(1, [3,4,5,6]) == False
+
+
+
+def findList(num, intList):
+    if len(intList) == 0:
+        return False
+    else:
+        if intList[0] == num:
+            return True
+        else:
+            return findList(num, intList[1:])
+
+assert findList(1, []) == False
+assert findList(3, [1, 2, 3]) == True
+assert findList(1, [3, 4, 5, 6]) == False
